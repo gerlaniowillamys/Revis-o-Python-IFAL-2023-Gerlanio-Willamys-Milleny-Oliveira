f1584a9160335dc611b04b61e7c013b9291b2f5b
gerlaniowillamys <138393805+gerlaniowillamys@users.noreply.github.com>
2023-07-03 21:44:22 -0300

    Delete Lista 1 directory

--- Lista 1/l1q1GM-523.py
+++
@@ -1,7 +0,0 @@
-#Lista de Exercício 1 - Questão 1 
-#Dupla: 2021316110 - Gerlânio Willamys e 2021317055 - Milleny Oliveira
-#Disciplina: Programação Web
-#Professor: Ítalo Arruda
-#Faça um Programa que mostre a mensagem "Alo mundo" na tela.
-
-print("Alô mundo")
